1e51259911184624d94dcfcaa915bed1ae02e8e4
shaqvendetta <shaquillemartin17@gmail.com>
2017-10-20 01:00:21 -0500

    Signed-off-by: shaqvendetta <shaquillemartin17@gmail.com>

---
+++ maze.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+
+ 	var a = document.getElementById("boundary1");
+
+ 	a.addEventListener("mouseover", function(){
+
+ 		a.setAttribute("class", "boundary youlose");
+
+ 	});	
+
+ };
